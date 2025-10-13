024d690036ee2f6775349a2db66af2ce99624889
Andrew Gubsky <andrei.gubsky@gmail.com>
2025-10-13 16:06:06 +0300

    Initial commit

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,21 @@
+
+
+/**
+ * 
+getImagesByQuery(query, page). 
+Ця функція повинна приймати два параметри 
+query (пошукове слово, яке є рядком) та 
+page (номер сторінки, яка є числом), 
+здійснювати HTTP-запит і повертати значення 
+властивості data з отриманої відповіді.
+
+ */
+
+export function getImagesByQuery(query, page) {
+
+}
+
+
+//const obj = { data: "" };
+
+export const data = {};
